6dc03b36c1e3040c99087b1fd58441ecc32e83c1
unknown <mhaseeb1604@gmail.com>
2024-01-08 14:06:11 +0500

    Users test cases optimized

--- test/test_users.py
+++ test/test_users.py
@@ -1,32 +1,28 @@
 import pytest
 
-# # Creating User
-# @pytest.mark.parametrize(
-#     "user_data, expected_status_code",
-#     [
-#         ({"name": "abc", "email": "abc@gmail.com", "password": "1234", "usertype": "normal"}, 201),
-#         ({"name": "haseeb", "email": "haseeb@gmail.com", "password": "1234", "usertype": "admin"}, 201),
-#         ({"name": "haseeb", "email": "abc3@gmail.com", "password": "1234", "usertype": "normal"}, 201),
-#         ({"name": "abc", "email": "abc2@example.com", "password": "abc123"}, 409),  # Existing email
-#         ({"email": "abc@example.com", "password": "abc123"}, 422),  # Missing fields
-#     ]
-# )
-# def test_create_users(client, test_user, user_data, expected_status_code):
-#     res = client.post("/users/", json=user_data)
+# Creating User
+@pytest.mark.parametrize(
+    "user_data, expected_status_code",
+    [
+        ({"name": "abc", "email": "abc@gmail.com", "password": "1234", "usertype": "normal"}, 201),
+        ({"name": "haseeb", "email": "haseeb@gmail.com", "password": "1234", "usertype": "admin"}, 201),
+        ({"name": "haseeb", "email": "abc3@gmail.com", "password": "1234", "usertype": "normal"}, 201),
+        ({"name": "abc", "email": "abc2@example.com", "password": "abc123"}, 409),  # Existing email
+        ({"email": "abc@example.com", "password": "abc123"}, 422),  # Missing fields
+    ]
+)
+def test_create_users(client, test_user, user_data, expected_status_code):
+    res = client.post("/users/", json=user_data)
     
-#     assert res.status_code == expected_status_code
+    assert res.status_code == expected_status_code
     
-#     if res.status_code == 422:
-#         assert res.json()["detail"][0]["type"] == "missing"
+    if res.status_code == 422:
+        assert res.json()["detail"][0]["type"] == "missing"
 
-
-
-import pytest
-
-# test_user_admin ID = 1
-# test_user       ID = 2
 
 @pytest.mark.parametrize(
+    # test_user_admin ID = 1
+    # test_user       ID = 2
     "client_fixture, user_id, expected_status_code",
     [
         ("authorized_client", 1, 200), #admin retriving his data
@@ -51,58 +47,48 @@ def test_get_users(client_fixture, user_id, expected_status_code, request, test_
 
     assert res.status_code == expected_status_code
 
-# def test_delete_user_by_admin(authorized_client, test_user_admin, test_user):
-#     res = authorized_client.delete(f"/users/{test_user['id']}")
-#     assert res.status_code == 204
+@pytest.mark.parametrize(
+    "client_fixture, user_id, expected_status_code",
+    [
+        ("authorized_client", "user_id", 204), #admin deleting user
+        ("authorized_client_normal", "user_id", 204), #normal user deleting his/her own account
+        ("authorized_client_normal", "user_id_admin", 403), #normal user deleting other
+        ("client", "user_id_admin", 401), #unautherized client deleting user
+        ("authorized_client", 999, 404), #authorized client deleting user not exist
+    ]
+)
+def test_delete_user(client_fixture, user_id, expected_status_code, request, test_user_admin, test_user):
+    client = request.getfixturevalue(client_fixture)
 
-# def test_delete_user_by_normal_user(authorized_client_normal, test_user_admin, test_user):
-#     res = authorized_client_normal.delete(f"/users/{test_user_admin['id']}")
-#     assert res.status_code == 403
+    if user_id == "user_id":
+        url = f"/users/{test_user['id']}"
+    elif user_id == "user_id_admin":
+        url = f"/users/{test_user_admin['id']}"
+    else:
+        url = f"/users/{user_id}"
 
-# def test_delete_user_by_unauthorized(client, test_user_admin, test_user):
-#     res = client.delete(f"/users/{test_user_admin['id']}")
-#     assert res.status_code == 401
+    res = client.delete(url)
+    assert res.status_code == expected_status_code
 
-# def test_delete_user_not_exist(authorized_client, test_user_admin, test_user):
-#     res = authorized_client.delete("/users/999")
-#     assert res.status_code == 404
 
-# def test_update_user_by_admin(authorized_client, test_user, test_user_admin):
-#     data = {
-#         "name": "abc1234",
-#         "email": "abc1234@example.com",
-#         "password": "abc123",
-#         "userType": "normal"
-#     }
-#     res = authorized_client.put(f"/users/{test_user['id']}", json=data)
-#     assert res.status_code == 201
+@pytest.mark.parametrize(
+    "client_fixture, user_id, data, expected_status_code",
+    [
+        ("authorized_client", "test_user_id", {"name": "abc1234", "email": "abc1234@example.com", "password": "abc123", "userType": "normal"}, 201), #admin updating user data
+        ("authorized_client_normal", "test_user_admin_id", {"name": "abc1234", "email": "abc1234@example.com", "password": "abc123", "userType": "normal"}, 403), # normal user updating other data
+        ("client", "test_user_admin_id", {"name": "abc1234", "email": "abc1234@example.com", "password": "abc123", "userType": "normal"}, 401), # unauthorized client updating data
+        ("authorized_client", 9999, {"name": "abc1234", "email": "abc1234@example.com", "password": "abc123", "userType": "normal"}, 404), # Non existent user update
+    ]
+)
+def test_update_user(client_fixture, user_id, data, expected_status_code, request, test_user, test_user_admin):
+    client = request.getfixturevalue(client_fixture)
 
-# def test_update_other_user_by_normal_user(authorized_client_normal, test_user, test_user_admin):
-#     data = {
-#         "name": "abc1234",
-#         "email": "abc1234@example.com",
-#         "password": "abc123",
-#         "userType": "normal"
-#     }
-#     res = authorized_client_normal.put(f"/users/{test_user_admin['id']}", json=data)
-#     assert res.status_code == 403
+    if user_id == "test_user_id":
+        url = f"/users/{test_user['id']}"
+    elif user_id == "test_user_admin_id":
+        url = f"/users/{test_user_admin['id']}"
+    else:
+        url = f"/users/{user_id}"
 
-# def test_update_other_user_by_unauthorized(client, test_user, test_user_admin):
-#     data = {
-#         "name": "abc1234",
-#         "email": "abc1234@example.com",
-#         "password": "abc123",
-#         "userType": "normal"
-#     }
-#     res = client.put(f"/users/{test_user_admin['id']}", json=data)
-#     assert res.status_code == 401
-
-# def test_update_non_existing_user(authorized_client, test_user, test_user_admin):
-#     data = {
-#         "name": "abc1234",
-#         "email": "abc1234@example.com",
-#         "password": "abc123",
-#         "userType": "normal"
-#     }
-#     res = authorized_client.put(f"/users/9999", json=data)
-#     assert res.status_code == 404
+    res = client.put(url, json=data)
+    assert res.status_code == expected_status_code
